52f999fcbc497a6667d6b02e86fb01aafcd2179b
Gourang Nagar <65488240+GourangNagar@users.noreply.github.com>
2022-01-20 03:10:49 +0530

    Update 1. Two Sum.cpp

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -10,3 +10,20 @@ public:
             return {};
     }
 };
+
+
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int,int> map;
+        for(int i = 0 ; i < n ; ++i)
+            if(map.find(target-nums[i]) == map.end())
+                map[nums[i]]=i;
+            else
+                return {map[target - nums[i]] , i};
+        return {};          
+    }
+};
