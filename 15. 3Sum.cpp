a92e518f43edc08b06257c2f1d17873a12189402
Gourang Nagar <65488240+GourangNagar@users.noreply.github.com>
2022-02-03 18:22:44 +0530

    Create 15. 3Sum.cpp

---
+++ 15. 3Sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& arr) {
+
+        int n = arr.size();
+        vector<vector<int> > vec;
+        
+        sort(arr.begin() , arr.end());
+
+        if (n<3) 
+            return vec;
+        if (arr[0]>0) 
+            return vec;
+
+        for(int l=0;l<n-2;i++)
+        {
+                    
+        int h=n-1;
+
+        while(l<h)
+        {
+            int k = -(arr[l] + arr[h]);
+            
+            if(binary_search(arr.begin()+l+1, arr.begin()+ h, k))
+                vec.push_back({arr[l],k,arr[h]});
+            
+            h--;
+            while(l<h && arr[h]==arr[h+1])
+                h--;     
+        }
+            
+        while(l<n-2 && arr[l]==arr[l+1])
+            l++;
+        
+        }
+        return vec;
+    }
+};
