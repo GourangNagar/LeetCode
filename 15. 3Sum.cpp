0719bfd395e81f428aa3e14aaddfc98cdf935155
Gourang Nagar <65488240+GourangNagar@users.noreply.github.com>
2022-02-03 19:08:29 +0530

    Update 15. 3Sum.cpp

--- 15. 3Sum.cpp
+++ 15. 3Sum.cpp
@@ -1,3 +1,4 @@
+// BOUND LEFT AND RIGHT THEN FIND THE REQD. VALUE IN BETWEEN OF L AND R THEN RETURN
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& arr) {
@@ -23,7 +24,7 @@ public:
             
             if(binary_search(arr.begin()+l+1, arr.begin()+ h, k))
                 vec.push_back({arr[l],k,arr[h]});
-            
+ 
             h--;
             while(l<h && arr[h]==arr[h+1])
                 h--;     
@@ -36,3 +37,53 @@ public:
         return vec;
     }
 };
+
+
+
+
+
+// FIX A THEN FIND B AND C BETWEEN ARRAY
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& arr) {
+        int n = arr.size();
+        sort(arr.begin() , arr.end());
+        vector<vector<int> > vec;
+        
+        if (n<3) return vec;
+        
+        for(int i=0; i<n-2 && arr[i]<=0 ;i++)
+        {
+        if(i>0 && arr[i]==arr[i-1])
+            continue;
+     
+        int l=i+1,h=n-1;
+
+        while(l<h)
+        {
+            int sum = arr[i] + arr[l] + arr[h];
+            
+            if(sum == 0) 
+            {
+                vec.push_back({arr[i],arr[l],arr[h]});
+                
+                // Work on Duplicates
+                while(l<h && arr[h]==arr[h-1])    h--;
+                while(l<h && arr[l]==arr[l+1])    l++;
+
+                h--;
+                l++;
+            }
+            
+            else if(sum < 0)   
+                l++;
+            else
+                h--;
+          }
+
+        }
+        return vec;
+    }
+};
+
+
