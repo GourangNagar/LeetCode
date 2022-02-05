5aa2618661d7de925e417230ba735afe3e227ac1
Gourang Nagar <65488240+GourangNagar@users.noreply.github.com>
2022-02-06 01:46:02 +0530

    Update 206. Reverse Linked List.cpp

--- 206. Reverse Linked List.cpp
+++ 206. Reverse Linked List.cpp
@@ -11,7 +11,7 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if(head==NULL)
+        if(head==NULL || head->next==NULL)
             return head;
 
         ListNode *curr = head;
