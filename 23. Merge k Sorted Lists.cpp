f7914a51c754ea698b9ef67c5776b86d62b923e1
Gourang Nagar <65488240+GourangNagar@users.noreply.github.com>
2022-02-06 04:19:29 +0530

    Update 23. Merge k Sorted Lists.cpp

--- 23. Merge k Sorted Lists.cpp
+++ 23. Merge k Sorted Lists.cpp
@@ -55,3 +55,41 @@ public:
         
     }
 };
+
+
+
+
+
+
+
+
+
+
+// OPTIMAL SOLUTION
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.empty()) return nullptr;
+        return mergeSort(lists, 0, lists.size()-1);
+    }
+    
+    ListNode* mergeSort(vector<ListNode*>& lists, int left, int right){
+        if(left>=right) return lists[left];
+        int mid = left + (right-left)/2;
+        ListNode* l1 = mergeSort(lists, left, mid);
+        ListNode* l2 = mergeSort(lists, mid+1, right);
+        return merge(l1, l2);
+    }
+    
+    ListNode* merge(ListNode* l1, ListNode* l2){
+        if(!l1) return l2;
+        if(!l2) return l1;
+        if(l1->val < l2->val){
+            l1->next = merge(l1->next, l2);
+            return l1;
+        }else {
+            l2->next = merge(l1, l2->next);
+            return l2;
+        }
+    }
+};
