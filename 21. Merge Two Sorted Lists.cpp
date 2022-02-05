c321bef995050a80bf92e2003367edcc851b206e
Gourang Nagar <65488240+GourangNagar@users.noreply.github.com>
2022-02-06 04:34:40 +0530

    Update 21. Merge Two Sorted Lists.cpp

--- 21. Merge Two Sorted Lists.cpp
+++ 21. Merge Two Sorted Lists.cpp
@@ -41,3 +41,34 @@ public:
         return temp->next;  
     }
 };
+
+
+
+
+
+
+
+
+// OPTIMAL SOLUTION VIA RECURSION
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* NewNode;
+        if(l1==NULL)
+            return l2;
+        if(l2==NULL)
+            return l1;
+        
+        if(l1->val < l2->val)
+        {
+            NewNode = l1;
+            NewNode->next = mergeTwoLists(l1->next,l2);
+        }
+        else
+        {
+            NewNode = l2;            
+            NewNode->next = mergeTwoLists(l1,l2->next);
+        } 
+        return NewNode;
+    }
+};
