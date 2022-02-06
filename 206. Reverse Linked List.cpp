85039325d63ce253fd2db464ccfca57653c6a8a5
Gourang Nagar <65488240+GourangNagar@users.noreply.github.com>
2022-02-06 23:52:12 +0530

    Update 206. Reverse Linked List.cpp

--- 206. Reverse Linked List.cpp
+++ 206. Reverse Linked List.cpp
@@ -1,13 +1,4 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// ITERATIVE METHOD
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
@@ -26,5 +17,27 @@ public:
         }
         head=prev;
         return head;
+    }
+};
+
+
+
+
+
+
+
+// USING RECURSION
+class Solution {
+public:
+    
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL || head->next==NULL)
+            return head;
+        
+        ListNode* newhead = reverseList(head->next);
+        ListNode* headnext = head->next;
+        headnext->next=head;
+        head->next = NULL;
+        return newhead;
     }
 };
